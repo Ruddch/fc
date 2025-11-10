0ef06f9e290ab4602b7a1a9f8cffce7ee4860275
Semion Shishlov <semionshish@gmail.com>
2025-11-10 18:58:03 +0100

    domen

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Если репозиторий не называется username.github.io, раскомментируйте и укажите имя репозитория:
+  // basePath: process.env.NODE_ENV === 'production' ? '/имя-репозитория' : '',
+  // trailingSlash: true,
 };
 
 export default nextConfig;
